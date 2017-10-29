e7917468efb498ef4fe875a72449c9ad2a30a83d
Zhou Yaoyang <diamondzyy@sina.com>
2017-10-29 22:38:34 +0800

    fix when # test > 30

--- adaboost_regressor.py
+++ adaboost_regressor.py
@@ -6,7 +6,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import RobustScaler
 from sklearn.svm import SVR
 
-all_classes = pd.read_csv('processed_2.csv',
+# all_classes = pd.read_csv('processed_2.csv',
+all_classes = pd.read_csv('special.csv',
         header=None, sep=',', encoding='gbk')
 
 groups = all_classes.groupby([0])
@@ -15,7 +16,7 @@ print('# mid-class: {}'.format(len(groups)))
 # scale train and test data to [-1, 1]
 def scale(train: np.array, test: np.array):
     # fit scaler
-    scaler = RobustScaler(with_scaling=True)
+    scaler = RobustScaler(with_scaling=False, with_centering=False)
     scaler = scaler.fit(train)
 
     train_scaled = scaler.transform(train)
@@ -38,12 +39,15 @@ def perform_on(model, train, test, scaler):
     model.fit(X_train, y_train)
 
     series = y_train
-    y_pred = X_train[-1]
+    y_pred = X_test[0]
 
     # 使用预测出的序列进行下一轮预测
     for i in range(0, len(X_test)):
+        # print('Predicting with:', y_pred)
+        # print('Expecting:', np.concatenate((X_test[i], y_test[i].reshape(1))))
         pred = model.predict(y_pred.reshape(1, -1))
         push(y_pred, pred)
+        # print('Got:', y_pred)
 
     y_pred = y_pred[-len(y_test):]
 
@@ -52,11 +56,9 @@ def perform_on(model, train, test, scaler):
     pred_origin = scaler.inverse_transform(predicted_matrix)[:, -1]
     test_origin = scaler.inverse_transform(test_matrix)[:, -1]
 
-    '''
     print('Predicted\t\tExpected')
     for a, b in zip(pred_origin, test_origin):
         print ('{:10.2f}\t\t{:10.2f}'.format(a, b))
-    '''
 
     mse = ((pred_origin - test_origin) ** 2).mean()
     print('MSE: {}'.format(mse))
@@ -66,9 +68,9 @@ for mid_class, group in groups:
     print('Current mid-class: {}'.format(mid_class))
 
     # split date:
-    group = group.drop([x for x in range(30)], axis=0)
+    # group = group.drop([x for x in range(30)], axis=0)
     group = group.drop([0], axis=1)
-    spliter = int(len(group) * 0.8)
+    spliter = int(len(group) - min(30, len(group) * 0.2))
     matrix = group.values
     train, test = matrix[0: spliter], matrix[spliter:]
     scaler, train, test = scale(train, test)
@@ -98,8 +100,8 @@ for mid_class, group in groups:
                 loss='ls',
                 ),
             RandomForestRegressor(
-                max_features=4,
-                max_depth=2,
+                max_features=15,
+                max_depth=3,
                 random_state=random_state,
                 )
             ]
